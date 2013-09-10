fcc408e234e13e4de27f7aa6a4b7190382ef9e1b
Bjorn Tipling <bjorn@ambientchill.com>
2013-09-10 05:10:05 -0700

    Stubbing out grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			all: ['Gruntfile.js', 'extension/js/dripmarks/**/*.js'],
+			options: {
+				curly: true,
+				unused: true,
+				onevar: true,
+				eqeqeq: true,
+				eqnull: true,
+				browser: true,
+				globals: {
+					jQuery: true
+				},
+			},
+		}
+  });
+
+
+  grunt.registerTask('default', ['jshint']);
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+};
