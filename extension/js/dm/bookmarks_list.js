7467b4c754320047c1e477f25ac1cb210d16bc9e
Bjorn Tipling <bjorn@ambientchill.com>
2013-09-15 14:12:44 -0700

    Stub out bookmarks list.

---
+++ extension/js/dm/bookmarks_list.js
@@ -0,0 +1,17 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone'
+], function($, _, Backbone) {
+
+  var Bookmarkslist;
+
+  /**
+   * @constructor
+   * @extends {Backbone.View}
+   */
+  Bookmarkslist = Backbone.View.extend({
+  });
+
+  return Bookmarkslist;
+});
